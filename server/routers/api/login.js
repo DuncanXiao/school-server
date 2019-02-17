7716ad22894e588a6ee0d4e8d90611d0e846fe88
xck_feimao@163.com <xck_feimao@163.com>
2019-02-18 00:46:52 +0800

    Add token verify

--- server/routers/api/login.js
+++ server/routers/api/login.js
@@ -2,6 +2,7 @@ import Router from 'koa-router';
 import validate from 'koa2-validation';
 import loginSchema from '../../lib/schema/login';
 import LoginController from '../../controllers/api/login';
+import {signToken} from '../../utilities/createJwt';
 
 const loginApi = new Router();
 
@@ -10,6 +11,7 @@ loginApi.post('/login', validate(loginSchema), async(ctx) => {
 	try {
 		const data = await loginController.login(ctx);
 		ctx.status = 200;
+		ctx.set('Authorization', signToken({studentId: data.get('id')}));
 		ctx.body = data;
 	} catch(error) {
 		loginController.handlerError(ctx, error);
--- server/routers/api/studentOrder.js
+++
@@ -1,4 +0,0 @@
-import Router from 'koa-router';
-import validate from 'koa2-validation';
-import studentSchema from '../../lib/schema/student';
-import StundentController from '../../controllers/api/student';
